babfb75b83c13803f41e6dc0f413f813e17df26f
Jannette Leatha <jannettl@re-w3.skd.21-school.uz>
2024-04-15 17:40:39 +0500

    Quest one completed

--- src/ai_module_2.sh
+++ src/ai_module_2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-
-$index 
-while IFS= read -r line
-do
-  index=$[ $index + 1 ]
-  if [ "$index" = "$line" ]; then
-    echo 'ЗАГРУЗКА ПОДМОДУЛЯ '"$line"' ПРОШЛА УСПЕШНО'
+index=0
+while IFS= read -r line; do
+  index=$((index + 1))
+  if [ "$index" -eq "$line" ]; then
+    echo "ЗАГРУЗКА ПОДМОДУЛЯ $line ПРОШЛА УСПЕШНО"
   else
-    echo 'ОШИБКА!ОШИБКА!ОШИБКА!ОШИБКА! Произошла ошибка при загрузке подмодуля '"$index"
+    echo "ОШИБКА! Произошла ошибка при загрузке подмодуля $index"
     exit 1
   fi
-done <  important_data_for_ai_module_2.txt
-if [ "$index" = 5 ];then
-  echo 'ЗАГРУЗКА ПОДМОДУЛЕЙ '"$line"' ПРОШЛА УСПЕШНО'
+done < important_data_for_ai_module_2.txt
+
+if [ "$index" -eq 3 ]; then
+  echo "ЗАГРУЗКА ПОДМОДУЛЕЙ ПРОШЛА УСПЕШНО"
 else
-  echo 'ОШИБКА!ОШИБКА!ОШИБКА!ОШИБКА! Не хватает нескольких модулей ... '"$index"
+  echo "ОШИБКА! Не хватает нескольких модулей ... $index"
   exit 1
 fi
 
-echo 'МОДУЛЬ 2 успешно загружен!'
+echo "МОДУЛЬ 2 успешно загружен!"
 exit 0
+
